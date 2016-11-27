248f52887d7232f049213648a5683458c71ff414
Anastasia Kvashnina <anastasia.kv97@gmail.com>
2016-11-27 15:41:49 +0600

    asd

--- Last homework/gulpfile.js
+++ Last homework/gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-var imagemin = require('gulp-imagemin');
 
 var path = {
     css:  'src/style/*.css',
     html: 'src/templates/*.html',
-    img: 'src/images/*.*',
+    vendor: {
+      css: 'src/vendor/css/*.css'
+    },
+    img: 'src/images/*',
     dist: {
       css:  'dist/style/',
       html: 'dist/',
+      vendor: 'dist/vendor/',
       img: 'dist/images/'
     }
 };
@@ -19,9 +24,23 @@ gulp.task('default', ['build', 'serve', 'watch']);
 
 gulp.task('css', function () {
   return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
     .pipe(concat('style.css'))
     .pipe(gulp.dest(path.dist.css));
 });
+
+gulp.task('css-min', function () {
+  return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
+    .pipe(concat('style.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.dist.css));
+});
+
 
 gulp.task('html', function () {
   return gulp.src(path.html)
@@ -30,17 +49,30 @@ gulp.task('html', function () {
 });
 
 gulp.task('img', function () {
-    return gulp.src(path.img) 
-        .pipe(imagemin())
-        .pipe(gulp.dest(path.dist.img)) //И бросим в build
-       
+  return gulp.src(path.img)
+    .pipe(gulp.dest(path.dist.img));
 });
 
-gulp.task('build', ['html', 'css', 'img']);
+gulp.task('vendor-css', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(path.dist.vendor));
+});
+
+gulp.task('vendor-css-min', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.dist.vendor));
+});
+
+gulp.task('build', ['html', 'css', 'vendor-css', 'img']);
+gulp.task('prod', ['html', 'css-min', 'vendor-css-min', 'img']);
 
 gulp.task('watch', function () {
   gulp.watch(path.css, ['css']);
   gulp.watch(path.html, ['html']);
+  gulp.watch(path.vendor.css, ['vendor-css']);
   gulp.watch(path.img, ['img']);
 });
 
@@ -50,6 +82,5 @@ gulp.task('serve', ['watch'], function() {
       baseDir: path.dist.html
     }
   });
-
   gulp.watch('dist/**').on('change', browserSync.reload);
 });
